b799a172c8a63f1d9b28bc4c4531e1ccbcc1247a
Anishdd5 <51048894+Anishdd5@users.noreply.github.com>
2019-06-03 18:46:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1629,4 +1629,25 @@ client.on('message', message => {
      
 });		 
 
+client.on('message', message => {
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == "اقتراح") {
+    if(!args.join(" ")) return message.channel.send(`**يرجي كتابة الاقتراح **`);
+    let channel = message.guild.channels.find(c => c.name == "اقتراحات");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setTitle(``)
+    .setFooter(`Select a reaction below to vote on suggestion`)
+    .setDescription(args.join(" "));
+    channel.send(embed).then(msg => {
+      msg.react("✅").then(() => msg.react("❌"));
+      message.delete()
+      message.channel.send(`**يرجي كتابة اقتراح لكي يتم ارساله الي روم الاقتراحات ❎ **`);
+    });
+  }
+});
+
 client.login(process.env.BOT_TOKEN)
