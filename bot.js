985043a973cd4132367e4e8c44c365bce03d6d0e
Anishdd5 <51048894+Anishdd5@users.noreply.github.com>
2019-06-06 12:45:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "460389855382470662") return;
+  if (message.author.id !== "484326398568300555") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
